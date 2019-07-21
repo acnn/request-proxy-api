189bc35f1ce6d8219043bb29a0528f776bcd2c01
Nithin Nayagam A C <acnnayagam@gmail.com>
2019-07-22 00:23:06 +0530

    Removing unused spec files

--- proxy-api/spec/factories/user_requests.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-    factory :todo do
-      client_id { Faker::Lorem.word }
-      url { Faker::Lorem.word }
-      headers { Faker::Lorem.word }
-      http_method { Faker::Lorem.word }
-      request_body { Faker::Lorem.word }
-    end
-  end
