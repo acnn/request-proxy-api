99663f27512656f6337fe8f969982fea1fa25c7d
Nithin Nayagam A C <acnnayagam@gmail.com>
2019-07-21 13:26:49 +0530

    Fixing overwritten format valdiation for url

--- proxy-api/app/models/user_request.rb
+++ proxy-api/app/models/user_request.rb
@@ -3,7 +3,7 @@ class UserRequest < ApplicationRecord
     URL_REGEXP = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
     HTTP_METHODS_LIST = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'CONNECT', 'TRACE', 'PATCH']
     validates :client_id, presence: true
-    validates :url, presence: true, format: { with: URL_REGEXP, multiline: true, message: "is invalid" }, 
-    format: { with: HTTPS_URL_REGEXP, multiline: true, message: "is invalid. Only HTTPS urls are supported." }  
+    validates :url, presence: true, format: { with: URL_REGEXP, multiline: true, message: "is invalid" } 
+    validates :url, format: { with: HTTPS_URL_REGEXP, multiline: true, message: "is invalid. Only HTTPS urls are supported." }  
     validates :http_method, presence: true, inclusion: { in: HTTP_METHODS_LIST, message: "is invalid or unsupported" }
 end
