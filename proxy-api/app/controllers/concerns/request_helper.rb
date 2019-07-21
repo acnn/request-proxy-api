3b1c4158727effc566c50a7a34a1a812fa2c5f48
Nithin Nayagam A C <acnnayagam@gmail.com>
2019-07-21 23:08:05 +0530

    Fixing issue in parsing request headers

--- proxy-api/app/controllers/concerns/request_helper.rb
+++ proxy-api/app/controllers/concerns/request_helper.rb
@@ -2,7 +2,7 @@ require 'http'
 module RequestHelper
     TIMEOUT_LIMIT = 5
     def make_request(user_request)
-      request_headers = JSON.parse(user_request.headers)[0] if !user_request.headers.blank?
+      request_headers = JSON.parse(user_request.headers) if !user_request.headers.blank?
       HTTP.timeout(TIMEOUT_LIMIT)
       .headers(request_headers)
       .send(user_request.http_method.downcase, *[user_request.url, :body => user_request.request_body])
