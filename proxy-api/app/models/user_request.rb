f67a7ff0b6a1cbe9fde2137c85ddc59c92968730
Nithin Nayagam A C <acnnayagam@gmail.com>
2019-07-24 23:23:30 +0530

    Refactoring model validation

--- proxy-api/app/models/user_request.rb
+++ proxy-api/app/models/user_request.rb
@@ -1,9 +1,9 @@
 class UserRequest < ApplicationRecord
-    HTTPS_URL_REGEXP = /^https/
-    URL_REGEXP = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+    URL_REGEXP = /^(https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
     HTTP_METHODS_LIST = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'CONNECT', 'TRACE', 'PATCH']
     validates :client_id, presence: true
-    validates :url, presence: true, format: { with: URL_REGEXP, multiline: true, message: "is invalid" } 
-    validates :url, format: { with: HTTPS_URL_REGEXP, multiline: true, message: "is invalid. Only HTTPS urls are supported." }  
-    validates :http_method, presence: true, inclusion: { in: HTTP_METHODS_LIST, message: "is invalid or unsupported" }
+    validates :url, presence: true
+    validates :url, format: { with: URL_REGEXP, multiline: true, message: "is either invalid or non-https" }, allow_blank: true
+    validates :http_method, presence: true
+    validates :http_method, inclusion: { in: HTTP_METHODS_LIST, message: "is either invalid or unsupported" }, allow_blank: true
 end
