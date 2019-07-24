79726bb9126b1cd7fe86a31af189688524b4c1a8
Nithin Nayagam A C <acnnayagam@gmail.com>
2019-07-25 00:03:12 +0530

    Removing wrong blocking period logic in rate limiter

--- proxy-api/app/controllers/concerns/rate_limiter.rb
+++ proxy-api/app/controllers/concerns/rate_limiter.rb
@@ -2,24 +2,19 @@ require 'redis'
 module RateLimiter
     TRACKING_PERIOD_SECONDS = 60
     TRACKING_PERIOD_REQUEST_LIMIT = 50
-    BLOCKING_PERIOD_SECONDS = 60
 
     def is_request_permitted(user_request)
-        count_key = 'count:' + user_request.client_id
-        blocked_key = 'blocked:' + user_request.client_id
-        if redis.get(blocked_key)
-            return false
-        end
-        if redis.get(count_key)
-            request_count = redis.incr(count_key)   
-            if request_count >= TRACKING_PERIOD_REQUEST_LIMIT
-                redis.set(blocked_key, 1)
-                redis.expire(blocked_key, BLOCKING_PERIOD_SECONDS)
+        client_key = user_request.client_id
+        request_count = redis.get(client_key)   
+        if request_count
+            if request_count.to_i >= TRACKING_PERIOD_REQUEST_LIMIT
+                return false
+            else
+                redis.incr(client_key)
             end
-        
         else
-            redis.set(count_key, 1)
-            redis.expire(count_key, TRACKING_PERIOD_SECONDS)
+            redis.set(client_key, 1)
+            redis.expire(client_key, TRACKING_PERIOD_SECONDS)
         end
         return true
     end
